82931a853de649e69727fc4838c4b532ea487989
Menaka S <menaka442@gmail.com>
2024-02-17 21:27:35 +0530

    add spring rest files from previous projects

--- src/main/java/com/zealtrack/employeecrud/dao/EmployeeRepository.java
+++ src/main/java/com/zealtrack/employeecrud/dao/EmployeeRepository.java
@@ -2,9 +2,7 @@ package com.zealtrack.employeecrud.dao;
 
 import com.zealtrack.employeecrud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //no need to write any code, all the required CRUD methods are provided by JPA itself
     //like magic methods
